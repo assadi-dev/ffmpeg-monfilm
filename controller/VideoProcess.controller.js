80c02f25a96fd4b9665a0cc36ed5c5a820344360
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-11-14 18:01:12 +0100

    ajustement reception des fichiers

--- controller/VideoProcess.controller.js
+++ controller/VideoProcess.controller.js
@@ -1,6 +1,9 @@
 import { fileInsvObject } from "../config/fileTest.config.js";
 import { ws } from "../index.js";
-import { generateGoprfilesObject } from "../services/Filestype.services.js";
+import {
+  generateGoprofilesObject,
+  generateInsvfilesObject,
+} from "../services/Filestype.services.js";
 import {
   full_process_gopro,
   full_process_insv,
@@ -8,22 +11,9 @@ import {
 import ObjectFileTest from "../services/ObjectFileTest.services.js";
 
 export const insv_process = (req, res) => {
-  const idProjectVideo = req.body?.idProjectVideo;
-  const room = req.body?.room;
-
-  const fileObject = req.body?.fileObject;
-  fileObject.room = room.toString();
-  fileObject.camera = req.body?.camera;
-
-  // full_process_insv(fileObject);
-
-  return res.json({ message: "processus en cours", fileObject });
-};
-
-export const gopro_process = (req, res) => {
   try {
-    const count = files.length;
     const { idProjectVideo, room, files, camera } = req.body;
+    const count = files.length;
 
     const filesProcess = [];
 
@@ -31,7 +21,39 @@ export const gopro_process = (req, res) => {
       fileObject.room = room.toString();
       fileObject.camera = camera;
 
-      const value = generateGoprfilesObject;
+      const value = generateInsvfilesObject(fileObject);
+
+      filesProcess.push(value);
+
+      full_process_insv(fileObject);
+    }
+
+    return res.json({
+      message: "processus en cours",
+      room,
+      camera,
+      filesProcess,
+      count,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error?.message,
+    });
+  }
+};
+
+export const gopro_process = (req, res) => {
+  try {
+    const { idProjectVideo, room, files, camera } = req.body;
+    const count = files.length;
+
+    const filesProcess = [];
+
+    for (const fileObject of files) {
+      fileObject.room = room.toString();
+      fileObject.camera = camera;
+
+      const value = generateGoprofilesObject(fileObject);
 
       filesProcess.push(value);
 
--- services/Filestype.services.js
+++ services/Filestype.services.js
@@ -5,12 +5,24 @@ export const getDelimiter = () => {
   else return "/";
 };
 
-export const generateGoprfilesObject = (fileObject) => {
+export const generateGoprofilesObject = (fileObject) => {
   if (fileObject) {
     return {
       id: fileObject.id,
       filename: fileObject.filename,
       camera: fileObject.camera,
+      progress: 0,
+    };
+  }
+};
+export const generateInsvfilesObject = (fileObject) => {
+  if (fileObject) {
+    return {
+      id: fileObject.id,
+      filename: fileObject.filename,
+      camera: fileObject.camera,
+      front: fileObject.front,
+      back: fileObject.back,
       progress: 0,
     };
   }
